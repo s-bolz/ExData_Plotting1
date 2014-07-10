69061d076d55f3aa0dc586fde7d173f14cae00cd
Sebastian Bolz <sebastian.bolz@immobilienscout24.de>
2014-07-10 17:45:03 +0200

    generation of plot 2

---
+++ plot2.R
@@ -0,0 +1,51 @@
+getData <- function() {
+    fieldNames <- names (
+        read.table (
+            file   = "household_power_consumption.txt",
+            header = TRUE,
+            sep    = ";",
+            nrows  = 1
+        )
+    )
+    data <- read.table (
+        file       = "household_power_consumption.txt",
+        sep        = ";",
+        col.names  = fieldNames,
+        na.strings = "?",
+        skip       = 66637,
+        nrows      = 2880
+    )
+    data$DateTime <- strptime (
+        paste (
+            data$Date,
+            data$Time,
+            sep = " "
+        ),
+        format = "%d/%m/%Y %H:%M:%S"
+    )
+    data
+}
+
+Sys.setlocale (
+    category = "LC_TIME",
+    locale   = "C"
+)
+data <- getData()
+png (
+    filename = "plot2.png",
+    width    = 480,
+    height   = 480,
+    units    = "px"
+)
+plot (
+    x    = data$DateTime,
+    y    = data$Global_active_power,
+    type = "n",
+    xlab = "",
+    ylab = "Global Active Power (kilowatts)"
+)
+lines (
+    x = data$DateTime,
+    y = data$Global_active_power
+)
+dev.off()
